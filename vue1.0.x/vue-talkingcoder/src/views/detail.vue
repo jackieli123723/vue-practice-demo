369607574a4eceb63dbc77022235334b3e76c009
李立冬 <380863274@qq.com>
2016-11-30 18:14:09 +0800

    vue联系 仿talkingcoder 没有热更新待修复

---
+++ vue1.0.x/vue-talkingcoder/src/views/detail.vue
@@ -0,0 +1,43 @@
+<template>
+	<div>
+		<me-header-tab title="详情页" router="/home"></me-header-tab>
+        <div class="me-container">
+        	<h3>详情页</h3>
+        	<p>{{Lcontent}}</p>
+        </div>
+   	</div>
+</template>
+<script>
+import MeHeaderTab from './me/MeHeaderTab.vue';
+	export default {
+		route: {
+		  data: function (transition) {
+
+		   this.$set('Lcontent',decodeURIComponent(this.$route.params.Lcontent));
+		  }
+		},
+		ready (){
+		     $.init();
+
+		  },
+		data (){
+			return {
+				Lcontent : ''
+			}
+		},
+		components:{
+				MeHeaderTab
+		}
+	}
+</script>
+
+
+<style scoped>
+	h3{
+		text-align: center;
+	}
+	p{
+		background: #fff;
+		padding: 2rem 0.5rem;
+	}
+</style>
---
+++ vue1.0.x/vue-talkingcoder/src/views/message.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+     <header-tab icon="settings" title="消息">
+
+    </header-tab>
+    <div class="img-area">
+      <img src="../../static/img/me/large.jpg">
+    </div>
+    <div class="hello">
+      <h1>{{ msg }}</h1>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import HeaderTab from '../components/HeaderTab.vue';
+export default {
+  data () {
+    return {
+      // note: changing this line won't causes changes
+      // with hot-reload because the reloaded component
+      // preserves its current state and we are modifying
+      // its initial state.
+      msg: 'message!'
+    }
+  },
+  components:{
+    HeaderTab
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+  h1 {
+    color: #42b983;
+  }
+  .img-area{
+    width: 100%;
+
+  }
+  .img-area img{
+    width: 100%;
+    height:9rem;
+    display: block;
+  }
+
+</style>
